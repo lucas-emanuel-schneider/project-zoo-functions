699684e76d303f0052ae3ccb6588042605a26de0
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-29 18:34:36 -0300

    terminando a 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName === 'undefined') return {};
+  return data.employees
+    .find((employ) => employ.firstName === employeeName || employ.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
