28bef726e28903f715a5b89d326e91e45c1247be
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-29 16:16:31 -0300

    primeiro exercicio

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => species
+  .filter((specie) => ids.some((idAnimal) => idAnimal === specie.id));
 
 module.exports = getSpeciesByIds;
