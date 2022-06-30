526606a11209fe2ca2794a8ee25cab4d00fb48e1
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-30 19:13:53 -0300

    getOldestfirst

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const pessoa = data.employees.find((person) => person.id === id);
+  const firstAnimalID = pessoa.responsibleFor[0];
+  const objAnimal = data.species.find((bicho) => bicho.id === firstAnimalID);
+  return objAnimal.residents.reduce((acc, resident) => {
+    if (resident.age > acc[2]) {
+      acc[0] = resident.name;
+      acc[1] = resident.sex;
+      acc[2] = resident.age;
+      return acc;
+    }
+    return acc;
+  }, [0, 0, 0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
