0a219443648235a06cf219fd3d5e92a3d5438f7c
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-29 22:27:30 -0300

    count pronta po pai

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, spec) => {
+      acc[spec.name] = spec.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.sex) {
+    return data.species
+      .find((ani2) => ani2.name === animal.specie).residents
+      .filter((mama) => mama.sex === animal.sex).length;
+  }
+  return data.species.find((ani) => ani.name === animal.specie).residents.length;
 }
 
 module.exports = countAnimals;
