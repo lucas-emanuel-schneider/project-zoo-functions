b371e1815aad88a6ee13c48760870b0b03c474ad
Lucas <lucas_emanuel_sc@hotmail.com>
2022-06-30 20:28:45 -0300

    começando test da opening

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,10 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('primeiro teste', () => {
+
+  });
+  it('segundo teste', () => {
+
+  });
+});
